9d008ba2dfd113f56110b7a7ad4e9a9d0a930917
julianort11 <ort.na140@gmail.com>
2025-03-13 10:17:24 -0500

    feat: :sparkles: location API consumption

--- weather-app-redesign/js/main.js
+++ weather-app-redesign/js/main.js
@@ -2,9 +2,19 @@
 import {searchWeather} from "./findApi.js";
 import ubication from "./myUbication.js";
 
+
+const search = document.querySelector("#search");
+
+const temp_c = document.querySelector("#temp_c");
+const icon = document.querySelector("#icon");
+const status = document.querySelector("#status");
+const last_updated = document.querySelector("#last_updated");
+const feelslike_c = document.querySelector("#feelslike_c");
+const maxtemp_c = document.querySelector("#maxtemp_c");
+const mintemp_c = document.querySelector("#mintemp_c");
+
 addEventListener("DOMContentLoaded", async(e)=>{
-    const response = await ubication();
-    
+    await ubication();
 })
 
 addEventListener("scroll", (e)=>{
@@ -55,34 +65,32 @@ addEventListener("scroll", (e)=>{
       }
 })
 
-const search = document.querySelector("#search");
-const temp_c = document.querySelector("#temp_c");
-const icon = document.querySelector("#icon");
-const status = document.querySelector("#status");
-const last_updated = document.querySelector("#last_updated");
-const feelslike_c = document.querySelector("#feelslike_c");
-const maxtemp_c = document.querySelector("#maxtemp_c");
-const mintemp_c = document.querySelector("#mintemp_c");
+
 
 search.addEventListener("submit", async(e)=>{
-     e.preventDefault();
-     const data = Object.fromEntries(new FormData(e.target));
-     const url = new URL(e.target.action);
-     data.enpoint = url.pathname;   
-     data.method = e.target.method;   
-     const response = await searchWeather(data);
-     if(response.status == 400){ return undefined};
-     console.log(response);
-     temp_c.textContent = `${response.current.temp_c}°`;
-     icon.src = response.current.condition.icon;
-     status.textContent = response.current.condition.text;
-     const fecha = new Date(response.current.last_updated).toLocaleDateString('en-US', { 
-          weekday: "long",  // Día de la semana completo (ej. "martes")
-          month: "long",    // Nombre completo del mes (ej. "marzo")
-          day: "numeric",   // Día del mes (ej. "11")
-          }).replace(" at", ",");
-     last_updated.textContent = fecha;
-     feelslike_c.textContent = `Feels like ${response.current.feelslike_c}°`
-     maxtemp_c.textContent = `Day ${response.forecast[0].day.maxtemp_c} °`
-     mintemp_c.textContent = `Night ${response.forecast[0].day.mintemp_c} °`
+     
+    alert("dfgh");
+    const data = Object.fromEntries(new FormData(e.target));
+    const url = new URL(e.target.action);
+    console.log(url);
+    data.enpoint = url.pathname;   
+    data.method = e.target.method;   
+    console.log(data);
+    
+    const response = await searchWeather(data);
+    if(response.status == 400){ return undefined};
+    console.log(response);
+    temp_c.textContent = `${response.current.temp_c}°`;
+    icon.src = response.current.condition.icon;
+    status.textContent = response.current.condition.text;
+    const fecha = new Date(response.current.last_updated).toLocaleDateString('en-US', { 
+         weekday: "long",  // Día de la semana completo (ej. "martes")
+         month: "long",    // Nombre completo del mes (ej. "marzo")
+         day: "numeric",   // Día del mes (ej. "11")
+         }).replace(" at", ",");
+    last_updated.textContent = fecha;
+    feelslike_c.textContent = `Feels like ${response.current.feelslike_c}°`
+    maxtemp_c.textContent = `Day ${response.forecast[0].day.maxtemp_c} °`
+    mintemp_c.textContent = `Night ${response.forecast[0].day.mintemp_c} °`
+    e.preventDefault();
 })
--- weather-app-redesign/js/myUbication.js
+++ weather-app-redesign/js/myUbication.js
@@ -1,4 +1,4 @@
-export default ()=>{
+export default async()=>{
     if ('geolocation' in navigator) {
         const success = async(position)=>{
             const {latitude, longitude} = position.coords;
@@ -11,19 +11,16 @@ export default ()=>{
             const response = await fetch(url.toString(), {method: "get"});
             const data = await response.json();
             const direccion = data.address;
-            return {
-                country: direccion.country,
-                region: direccion.state,
-                county: `${direccion.county || direccion.state_district}`,
-                municipality: `${direccion.city || direccion.town || direccion.village}`
-            }
+            const input = document.querySelector("#search .div__icon input");
+            input.value = `${direccion.city || direccion.town || direccion.village}, ${direccion.county || direccion.state_district}, ${direccion.state}, ${direccion.country}`
+            const search = document.querySelector("#search");
+            search.dispatchEvent(new Event("submit"));
         }
-
         const error = async(err) => {
-            console.warn(`ERROR(${err.code}): ${err.message}`);
+            return `ERROR(${err.code}): ${err.message}`
         }
 
-        navigator.geolocation.getCurrentPosition(success, error);
+         navigator.geolocation.getCurrentPosition(success, error);
 
     } else {
     console.log('La geolocalización NO está disponible en este navegador');
